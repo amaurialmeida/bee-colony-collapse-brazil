9e416edb5963c56dbb5f1755297dba43e4f140b7
amaurialmeida <33519484+amaurialmeida@users.noreply.github.com>
2026-05-08 22:59:10 +0000

    atualizações dos direitos

--- app.py
+++ app.py
@@ -513,6 +513,6 @@ with col_raio2:
 # Rodapé
 st.markdown("---")
 st.markdown(
-    "<p style='text-align: center; color: gray;'>🐝 <b>Trabalho de Conclusão de Curso - Amauri Almeida - Brasil<a href='https://emojitool.com/pt/flag-for-brazil'>🇧🇷</a> </b><br>Dados obtidos do Trabalho de Conclusão do Curso de Gestão Ambiental - FATEC - Faculdade de Tecnologia Deputado Ary Fossen Nota Máxima no ENADE </b><br> Pós-Graduação em Apicultura e Meliponicultura da UNITAU - Universidade de Taubaté <br>📍 Minas Gerais | São Paulo | Paraná | Rio Grande do Sul 🌍</p>",
+    "<p style='text-align: center; color: gray;'>🐝 <b>Trabalho de Conclusão de Curso - Amauri Almeida - Brasil 🇧🇷 </b><br>Dados obtidos do Trabalho de Conclusão do Curso de Gestão Ambiental - FATEC - Faculdade de Tecnologia Deputado Ary Fossen - NOTA MÁXIMA NO ENADE 2023/2025 </b><br> Pós-Graduação em Apicultura e Meliponicultura da UNITAU - Universidade de Taubaté <br>📍 Minas Gerais | São Paulo | Paraná | Rio Grande do Sul 🌍</p>",
     unsafe_allow_html=True
 )
